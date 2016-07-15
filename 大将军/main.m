08c87f84e0a06bf9d08b0d7178431eb296e5be8f
SingYi <23323231@qq.com>
2016-07-15 11:09:26 +0800

    Initial Commit

---
+++ 大将军/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  大将军
+//
+//  Created by 石燚 on 16/7/15.
+//  Copyright © 2016年 SingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
