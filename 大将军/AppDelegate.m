55509df65290765ccbbf1e24cea9d18c62b44001
SingYi <23323231@qq.com>
2016-07-15 17:48:23 +0800

    更换了根控制器

--- 大将军/AppDelegate.m
+++ 大将军/AppDelegate.m
@@ -10,11 +10,11 @@
 
 #import "MainTabbarController.h"
 #import "LoginViewController.h"
-//#import "UMSocial.h"
-//#import "UMSocialQQHandler.h"
-//#import "UMSocialSinaSSOHandler.h"
-//#import "UMSocialWechatHandler.h"
-//#import <CoreData/CoreData.h>
+#import "UMSocial.h"
+#import "UMSocialQQHandler.h"
+#import "UMSocialSinaSSOHandler.h"
+#import "UMSocialWechatHandler.h"
+#import <CoreData/CoreData.h>
 
 @interface AppDelegate ()
 
@@ -26,16 +26,27 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     
-    LoginViewController *mainVc = [[LoginViewController alloc]init];
+    MainTabbarController *mainVc = [[MainTabbarController alloc]init];
     
     self.window.rootViewController = mainVc;
     //    self.window.userInteractionEnabled = YES;
     
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:mainVc];
-    
-    self.window.rootViewController = nav;
+//    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:mainVc];
+//    
+//    self.window.rootViewController = nav;
     
     [self.window makeKeyAndVisible];
+    
+#pragma mark - 配置第三方APPID
+    //设置友盟社会化组件appkey
+    [UMSocialData setAppKey:Appkey];
+    //设置微信AppId、appSecret，分享url
+    [UMSocialWechatHandler setWXAppId:@"wxd930ea5d5a258f4f" appSecret:@"db426a9829e4b49a0dcac7b4162da6b6" url:@"http://www.umeng.com/social"];
+    //设置手机QQ 的AppId，Appkey，和分享URL，需要#import "UMSocialQQHandler.h"
+    [UMSocialQQHandler setQQWithAppId:@"100424468" appKey:@"c7394704798a158208a74ab60104f0ba" url:@"http://www.umeng.com/social"];
+    //打开新浪微博的SSO开关，设置新浪微博回调地址，这里必须要和你在新浪微博后台设置的回调地址一致。需要 #import "UMSocialSinaSSOHandler.h"
+    [UMSocialSinaSSOHandler openNewSinaSSOWithAppKey:@"3050076814"
+                                              secret:@"b06eea4f9b73a3e8f6eaeb4e5cbda3b4" RedirectURL:@"http://weibo.com/u/2456110315/home?wvr=5"];
     
     return YES;
 }
